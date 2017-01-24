4196cf8e193788d1851626b7fcf4f40d23e2d4d6
Thiago Inocêncio <inocencio.thiago@hotmail.com>
2017-01-23 22:27:29 -0200

    Add files via upload

---
+++ Linguagem C/Lista 8 - Recursao/11.c
@@ -0,0 +1,20 @@
+﻿// 11º Exercício da lista de recursão
+
+#include <stdio.h>
+
+void imprimeArray(int *arr, int n) {
+
+    if(n == 0) return;
+    prinf("%d " , arr[n]);
+    return imprimeArray(arr, n-1);
+
+}
+
+int main() {
+
+    int arr[10] = {4, 5, 8, 65, 333, 21, 65, 7, 54, 2};
+
+    imprimeArray(9);
+
+    return 0;
+}
