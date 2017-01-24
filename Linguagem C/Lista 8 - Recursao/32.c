22f26508d91627772f70f3cd761083a0da096602
Thiago Inocêncio <inocencio.thiago@hotmail.com>
2017-01-24 12:00:46 -0200

    Add files via upload

---
+++ Linguagem C/Lista 8 - Recursao/32.c
@@ -0,0 +1,21 @@
+// 32º Exercício da lista de recursão
+
+#include <stdio.h>
+
+int binario(int n) {
+
+    if(n > 1) binario (n/2);
+
+    if(n % 2 != 0) printf(" 1 ");
+      else printf(" 0 ");
+
+    return;
+
+}
+
+int main() {
+
+    binario(233);
+    
+    return 0;
+}
